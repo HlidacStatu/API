a7a2caa9df1ed2e1ce4999ea99818607264be8ec
Michal Blaha <michal@michalblaha.cz>
2018-10-30 19:16:54 +0100

    Added missing API methods
    + GetItemFromDataset
    + SearchItemsInDataset

--- DatasetConnector/DatasetConnector/DatasetConnector.cs
+++ DatasetConnector/DatasetConnector/DatasetConnector.cs
@@ -5,26 +5,32 @@ using System.Threading.Tasks;
 
 namespace HlidacStatu.Api.Dataset.Connector
 {
-	public class DatasetConnector : IDatasetConnector
-	{
-		private static string apiRoot = "https://www.hlidacstatu.cz/api/v1";
+    public class DatasetConnector : IDatasetConnector
+    {
+        private static string apiRoot = "https://www.hlidacstatu.cz/api/v1";
 
-		private readonly string ApiToken;
-		private readonly HttpClient HttpClient;
+        private readonly string ApiToken;
+        private readonly HttpClient HttpClient;
 
-		public DatasetConnector(string apiToken)
-		{
-			ApiToken = apiToken;
+        public DatasetConnector(string apiToken)
+        {
+            ApiToken = apiToken;
 
-			HttpClient = new HttpClient();
-			HttpClient.DefaultRequestHeaders.Add("Authorization", ApiToken);
-		}
+            HttpClient = new HttpClient();
+            HttpClient.DefaultRequestHeaders.Add("Authorization", ApiToken);
+        }
 
-		public async Task<string> AddItemToDataset<TData>(Dataset<TData> dataset, TData item)
+        public async Task<string> AddItemToDataset<TData>(Dataset<TData> dataset, TData item)
+            where TData : IDatasetItem
+        {
+            return await AddItemToDataset<TData>(dataset.DatasetId, item);
+        }
+
+        public async Task<string> AddItemToDataset<TData>(string datasetId, TData item)
 			where TData : IDatasetItem
 		{
 			var content = new StringContent(JsonConvert.SerializeObject(item));
-			var response = await HttpClient.PostAsync(apiRoot + $"/DatasetItem/{dataset.DatasetId}/{item.Id}", content);
+			var response = await HttpClient.PostAsync(apiRoot + $"/DatasetItem/{datasetId}/{item.Id}", content);
 			var result = JObject.Parse(await response.Content.ReadAsStringAsync());
 			if (result["error"] == null)
 			{
@@ -35,8 +41,7 @@ namespace HlidacStatu.Api.Dataset.Connector
 				throw new DatasetConnectorException(result["error"]["description"].Value<string>());
 			}
 		}
-
-		public async Task<bool> DatasetExists<TData>(Dataset<TData> dataset)
+        public async Task<bool> DatasetExists<TData>(Dataset<TData> dataset)
 			where TData : IDatasetItem
 		{
 			var response = await HttpClient.GetAsync(apiRoot + "/Datasets/" + dataset.DatasetId);
@@ -100,5 +105,36 @@ namespace HlidacStatu.Api.Dataset.Connector
 			await deleteResponse.Content.ReadAsStringAsync();
 			return;
 		}
-	}
+
+        public async Task<TData> GetItemFromDataset<TData>(string datasetId, string id) where TData : IDatasetItem
+        {
+            //var content = new StringContent(JsonConvert.SerializeObject(item));
+            var response = await HttpClient.GetAsync(apiRoot + $"/DatasetItem/{datasetId}/{id}");
+            var result = JObject.Parse(await response.Content.ReadAsStringAsync());
+            if (result["error"] == null)
+            {
+                return result.ToObject<TData>();
+            }
+            else
+            {
+                throw new DatasetConnectorException(result["error"]["description"].Value<string>());
+            }
+        }
+
+
+        public async Task<SearchResult<TData>> SearchItemsInDataset<TData>(string datasetId, string query, int page) where TData : IDatasetItem
+        {
+            //var content = new StringContent(JsonConvert.SerializeObject(item));
+            var response = await HttpClient.GetAsync(apiRoot + $"/DatasetSearch/{datasetId}?q={System.Net.WebUtility.UrlEncode(query)}&page={page}");
+            var result = JObject.Parse(await response.Content.ReadAsStringAsync());
+            if (result["error"] == null)
+            {
+                return result.ToObject<SearchResult<TData>>();
+            }
+            else
+            {
+                throw new DatasetConnectorException(result["error"]["description"].Value<string>());
+            }
+        }
+    }
 }
